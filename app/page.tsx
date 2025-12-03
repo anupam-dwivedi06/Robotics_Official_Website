4d0dc9fdd5018aa226e1773226a83d3940bb9605
anupam-dwivedi05 <anupamdwivedi8318@gmail.com>
2025-12-03 22:40:01 +0530

    working on the about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from './Components/Navbar'
 import Hero from './Components/HeroSection'
+import About from './Components/About'
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 }
